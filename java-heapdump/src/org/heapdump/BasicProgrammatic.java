d7a0d2a1142c26a49c1b61c5f89b3ff89d96dc1e
Steve Lewis <stelewis@redhat.com>
2023-02-21 08:07:03 -0600

    Basic single threaded heap dump.

---
+++ java-heapdump/src/org/heapdump/BasicProgrammatic.java
@@ -0,0 +1,21 @@
+package org.heapdump;
+
+import com.sun.management.HotSpotDiagnosticMXBean;
+
+import javax.management.MBeanServer;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+public class BasicProgrammatic {
+
+    public static void main(String[] args) throws IOException {
+        dumpHeap("basic-programmatic.hprof", true);
+    }
+
+    public static void dumpHeap(String filePath, boolean live) throws IOException {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(
+                server, "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+        mxBean.dumpHeap(filePath, live);
+    }
+}
