f61910e5cecfefb83a913be7f40484eb8ef1e413
Steve Lewis <stelewis@redhat.com>
2023-02-28 08:09:50 -0600

    Add a java class to retrieve the heap dump non-programmatically.

---
+++ java-heapdump/src/org/heapdump/BasicInteractive.java
@@ -0,0 +1,12 @@
+package org.heapdump;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BasicInteractive {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("You can retrieve the heap dump now. Press any key to exit this program.");
+        System.in.read();
+    }
+}
---
+++ jmap.sh
@@ -0,0 +1,1 @@
+jmap -dump:live,format=b,file=/tmp/dump.hprof $1
